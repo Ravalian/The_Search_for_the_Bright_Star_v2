4fe770deaf6893959c3579ddfe0a84069c480c10
René Iversen <20112809@post.au.dk>
2021-11-20 13:54:01 +0100

    Added frame invariant movement speed

--- The_Search_for_the_Bright_Star_v2/Assets/Scripts/LenzController.cs
+++ The_Search_for_the_Bright_Star_v2/Assets/Scripts/LenzController.cs
@@ -20,8 +20,8 @@ public class LenzController : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         Vector2 position = transform.position;
-        position.x = position.x + 0.01f * horizontal;
-        position.y = position.y + 0.01f * vertical;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
         transform.position = position;
     }
