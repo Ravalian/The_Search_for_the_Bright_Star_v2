62582d034e05c9acb5c0cdb4e1becd5c8965a862
Alexander Hoffmann <Alexander.wh@hotmail.com>
2021-11-23 10:44:17 +0100

    Working on adding projectiles

--- The_Search_for_the_Bright_Star_v2/Assets/Scripts/LenzController.cs
+++ The_Search_for_the_Bright_Star_v2/Assets/Scripts/LenzController.cs
@@ -7,15 +7,20 @@ using UnityEngine;
  */
 public class LenzController : MonoBehaviour
 {
-    private Rigidbody2D rigidbody2D;
+    private Rigidbody2D rigidbody2d;
     private float horizontal;
     private float vertical;
+
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    public float speed = 3.0f;
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         //Gets RigidBody2D attached to the gameobject the script is attached to
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called every frame
@@ -26,16 +31,38 @@ public class LenzController : MonoBehaviour
         //Get horizontal and vertical movement from pressing left or right keys
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C)) {
+            Launch();
+        }
     }
 
     void FixedUpdate()
     {
-        var position = rigidbody2D.position;
+        var position = rigidbody2d.position;
 
         //Time.deltaTime - make movement not dependent on frames
-        position.x += 3.0f * horizontal * Time.deltaTime; 
-        position.y += 3.0f * vertical * Time.deltaTime;
+        position.x += speed * horizontal * Time.deltaTime; 
+        position.y += speed * vertical * Time.deltaTime;
 
-        rigidbody2D.MovePosition(position);
+        rigidbody2d.MovePosition(position);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 180);
+
+        //animator.SetTrigger("Launch");
     }
 }
