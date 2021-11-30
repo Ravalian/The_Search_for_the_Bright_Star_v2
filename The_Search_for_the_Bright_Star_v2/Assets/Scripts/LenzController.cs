ae43526e6981a1c81d8550db0ad2dc27e50623c0
Nicolai Dreves Nielsen <Nicolai_Dreves1994@hotmail.dk>
2021-11-30 11:01:29 +0100

    interactable

--- The_Search_for_the_Bright_Star_v2/Assets/Scripts/LenzController.cs
+++ The_Search_for_the_Bright_Star_v2/Assets/Scripts/LenzController.cs
@@ -48,6 +48,14 @@ public class LenzController : MonoBehaviour
 
         animator.SetFloat("MoveX", horizontal);
         animator.SetFloat("MoveY", vertical);
+
+        if (Input.GetKeyDown(KeyCode.V)) {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("InteractableObject"));
+            if(hit.collider != null)
+            {
+                Debug.Log("Raycast has hit the obejct " + hit.collider.gameObject);
+            }
+        }
     }
 
     private void FixedUpdate()
